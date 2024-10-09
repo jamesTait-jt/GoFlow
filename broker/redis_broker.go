b10bd353e1cdf53b35e4d58ce8685c6d9105ee9e
jamesTait-jt <jamesatait12@gmail.com>
2024-10-09 21:46:44 +0100

    Comment out redis broker

--- broker/redis_broker.go
+++ broker/redis_broker.go
@@ -1,24 +1,24 @@
 package broker
 
-import (
-	"github.com/jamesTait-jt/GoFlow/internal/task"
-	"github.com/redis/go-redis/v9"
-)
+// import (
+// 	"github.com/jamesTait-jt/GoFlow/internal/task"
+// 	"github.com/redis/go-redis/v9"
+// )
 
-type RedisBroker struct {
-	client      *redis.Client
-	taskChannel chan<- task.Task
-}
+// type RedisBroker struct {
+// 	client      *redis.Client
+// 	taskChannel chan<- task.Task
+// }
 
-func NewRedisBroker(redisAddr string, taskChannel chan<- task.Task) *RedisBroker {
-	client := redis.NewClient(&redis.Options{Addr: redisAddr})
+// func NewRedisBroker(redisAddr string, taskChannel chan<- task.Task) *RedisBroker {
+// 	client := redis.NewClient(&redis.Options{Addr: redisAddr})
 
-	broker := &RedisBroker{
-		client:      client,
-		taskChannel: taskChannel,
-	}
-}
+// 	broker := &RedisBroker{
+// 		client:      client,
+// 		taskChannel: taskChannel,
+// 	}
+// }
 
-func (rd *RedisBroker) Submit(t task.Task) {
+// func (rd *RedisBroker) Submit(t task.Task) {
 
-}
+// }
