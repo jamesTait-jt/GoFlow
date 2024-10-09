42aebb355859fe1f6989fc502736020111a00ad5
jamesTait-jt <jamesatait12@gmail.com>
2024-10-09 19:18:44 +0100

    Add unit tests for updated worker

--- internal/worker/worker_test.go
+++ internal/worker/worker_test.go
@@ -15,96 +15,61 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// mockTaskHandlerRegistry is a mock for the task registry interface
-type mockTaskHandlerRegistry struct {
+type mockTaskSource struct {
 	mock.Mock
+	// taskChan chan task.Task
 }
 
-func (m *mockTaskHandlerRegistry) GetHandler(taskType string) (task.Handler, bool) {
-	args := m.Called(taskType)
-	if args.Get(0) == nil {
-		return nil, args.Bool(1) // Return nil as the task handler
-	}
-
-	return args.Get(0).(task.Handler), args.Bool(1)
+func (m *mockTaskSource) Dequeue() <-chan task.Task {
+	args := m.Called()
+	return args.Get(0).(chan task.Task)
 }
 
 func TestNewWorker_Creates_a_new_worker_with_variables_initialised(t *testing.T) {
 	// Arrange
-	queue := make(<-chan task.Task)
-	registry := new(mockTaskHandlerRegistry)
+	taskChan := make(chan task.Task)
+	defer close(taskChan)
+
+	taskSource := mockTaskSource{}
 
 	// Act
-	w := NewWorker(1, queue, registry)
+	w := NewWorker(1, &taskSource)
 
 	// Assert
 	assert.Equal(t, 1, w.id)
-	assert.Equal(t, queue, w.queue)
-	assert.Equal(t, registry, w.taskHandlerRegistry)
+	assert.Equal(t, &taskSource, w.tasks)
 }
 
 func TestWorker_Start(t *testing.T) {
 	t.Run("Processes a task successfully", func(t *testing.T) {
 		// Arrange
-		queue := make(chan task.Task)
-		registry := new(mockTaskHandlerRegistry)
+		taskChan := make(chan task.Task)
+		defer close(taskChan)
 
-		taskToProcess := task.Task{
-			ID:      "task-1",
-			Type:    "test-task",
-			Payload: "test-payload",
-		}
+		taskSource := mockTaskSource{}
+		taskSource.On("Dequeue").Return(taskChan).Twice()
 
 		var receivedPayload string
 
-		handler := task.Handler(func(payload any) error {
+		expectedResult := task.Result{
+			Payload: "payload",
+			Error:   nil,
+		}
+		handler := task.Handler(func(payload any) task.Result {
 			receivedPayload, _ = payload.(string)
-			return nil
+			return expectedResult
 		})
 
-		registry.On("GetHandler", taskToProcess.Type).Return(handler, true).Once()
-
-		w := NewWorker(1, queue, registry)
-
-		ctx, cancel := context.WithCancel(context.Background())
-
-		var wg sync.WaitGroup
-
-		wg.Add(1)
-
-		// Act
-		w.Start(ctx, &wg)
-		queue <- taskToProcess
-
-		cancel()
-
-		// Assert
-		wg.Wait()
-
-		registry.AssertExpectations(t)
-
-		assert.Equal(t, taskToProcess.Payload, receivedPayload)
-	})
-
-	t.Run("Logs warning and skips task when no handler is registered", func(t *testing.T) {
-		// Arrange
-		var logOutput bytes.Buffer
-
-		logrus.SetOutput(&logOutput) // Capture log output
-		logrus.SetLevel(logrus.WarnLevel)
-
-		queue := make(chan task.Task)
-		registry := new(mockTaskHandlerRegistry)
-
+		resultCh := make(chan task.Result)
 		taskToProcess := task.Task{
-			ID:      "task-1",
-			Type:    "test-task",
-			Payload: "test-payload",
+			ID:       "task-1",
+			Type:     "test-task",
+			Payload:  "test-payload",
+			Handler:  handler,
+			ResultCh: resultCh,
 		}
 
-		registry.On("GetHandler", taskToProcess.Type).Return(nil, false).Once()
-
-		w := NewWorker(1, queue, registry)
+		w := NewWorker(1, &taskSource)
 
 		ctx, cancel := context.WithCancel(context.Background())
 
@@ -114,48 +79,55 @@ func TestWorker_Start(t *testing.T) {
 
 		// Act
 		w.Start(ctx, &wg)
-		queue <- taskToProcess
+		taskChan <- taskToProcess
+
+		result := <-resultCh
 
 		cancel()
 
 		// Assert
 		wg.Wait()
 
-		registry.AssertExpectations(t)
+		taskSource.AssertExpectations(t)
 
-		require.Contains(t, logOutput.String(), "No handler registered for task type")
-		require.Contains(t, logOutput.String(), "worker_id=1")
-		require.Contains(t, logOutput.String(), "task_type=test-task")
+		assert.Equal(t, expectedResult, result)
+		assert.Equal(t, taskToProcess.Payload, receivedPayload)
 	})
 
 	t.Run("Logs error when handler reports error", func(t *testing.T) {
 		// Arrange
 		var logOutput bytes.Buffer
 
-		logrus.SetOutput(&logOutput) // Capture log output
+		logrus.SetOutput(&logOutput)
 		logrus.SetLevel(logrus.ErrorLevel)
 
-		queue := make(chan task.Task)
-		registry := new(mockTaskHandlerRegistry)
+		taskChan := make(chan task.Task)
+		defer close(taskChan)
 
-		taskToProcess := task.Task{
-			ID:      "task-1",
-			Type:    "test-task",
-			Payload: "test-payload",
-		}
-
-		errorToReturn := errors.New("error")
+		taskSource := mockTaskSource{}
+		taskSource.On("Dequeue").Return(taskChan).Twice()
 
 		var receivedPayload string
 
-		handler := task.Handler(func(payload any) error {
+		expectedResult := task.Result{
+			Payload: nil,
+			Error:   errors.New("handler error"),
+		}
+		handler := task.Handler(func(payload any) task.Result {
 			receivedPayload, _ = payload.(string)
-			return errorToReturn
+			return expectedResult
 		})
 
-		registry.On("GetHandler", taskToProcess.Type).Return(handler, true).Once()
+		resultCh := make(chan task.Result)
+		taskToProcess := task.Task{
+			ID:       "task-1",
+			Type:     "test-task",
+			Payload:  "test-payload",
+			Handler:  handler,
+			ResultCh: resultCh,
+		}
 
-		w := NewWorker(1, queue, registry)
+		w := NewWorker(1, &taskSource)
 
 		ctx, cancel := context.WithCancel(context.Background())
 
@@ -165,21 +137,23 @@ func TestWorker_Start(t *testing.T) {
 
 		// Act
 		w.Start(ctx, &wg)
+		taskChan <- taskToProcess
 
-		queue <- taskToProcess
+		result := <-resultCh
 
 		cancel()
 
 		// Assert
 		wg.Wait()
 
-		registry.AssertExpectations(t)
+		taskSource.AssertExpectations(t)
 
 		assert.Equal(t, taskToProcess.Payload, receivedPayload)
+		assert.Equal(t, expectedResult, result)
 
 		require.Contains(t, logOutput.String(), "Failed to process task")
 		require.Contains(t, logOutput.String(), "worker_id=1")
 		require.Contains(t, logOutput.String(), "task_id=task-1")
-		require.Contains(t, logOutput.String(), fmt.Sprintf("error=%s", errorToReturn))
+		require.Contains(t, logOutput.String(), fmt.Sprintf("error=%q", expectedResult.Error))
 	})
 }
