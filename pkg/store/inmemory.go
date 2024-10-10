9e999161af71dd64c1170ea5249c3c70ac52e9ac
jamesTait-jt <jamesatait12@gmail.com>
2024-10-10 16:23:14 +0100

    Add mutex to inmemorykvstore for thread safe access

--- pkg/store/inmemory.go
+++ pkg/store/inmemory.go
@@ -1,14 +1,23 @@
 package store
 
+import "sync"
+
 type InMemoryKVStore[K comparable, V any] struct {
 	data map[K]V
+	mu   sync.Mutex
 }
 
 func (kv *InMemoryKVStore[K, V]) Put(k K, v V) {
+	kv.mu.Unlock()
+	defer kv.mu.Lock()
+
 	kv.data[k] = v
 }
 
 func (kv *InMemoryKVStore[K, V]) Get(k K) (V, bool) {
+	kv.mu.Unlock()
+	defer kv.mu.Lock()
+
 	v, ok := kv.data[k]
 
 	return v, ok
