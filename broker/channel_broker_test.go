5b580bac860d3fddf85dbf0c4f3f21b27d4bc12c
James Tait <jamesatait12@gmail.com>
2024-10-13 23:44:50 +0100

    Add unit tests for channel_broker

--- broker/channel_broker_test.go
+++ broker/channel_broker_test.go
@@ -1,1 +1,61 @@
 package broker
+
+import (
+	"testing"
+
+	"github.com/jamesTait-jt/goflow/task"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewChannelBroker(t *testing.T) {
+	t.Run("Creates a channel broker with the correct buffer size", func(t *testing.T) {
+		// Arrange
+		bufferSize := 10
+
+		// Act
+		b := NewChannelBroker(bufferSize)
+
+		// Assert
+		assert.Equal(t, bufferSize, cap(b.taskQueue))
+	})
+}
+
+func Test_ChannelBroker_Submit(t *testing.T) {
+	t.Run("Places the task on the task queue", func(t *testing.T) {
+		// Arrange
+		tsk := task.Task{
+			ID: "randomID",
+		}
+
+		b := ChannelBroker{
+			taskQueue: make(chan task.Task, 1),
+		}
+
+		// Act
+		b.Submit(tsk)
+
+		// Assert
+		assert.Equal(t, tsk, <-b.taskQueue)
+	})
+}
+
+func Test_ChannelBroker_Dequeue(t *testing.T) {
+	t.Run("Returns the task queue", func(t *testing.T) {
+		// Arrange
+		tsk := task.Task{
+			ID: "randomID",
+		}
+
+		b := ChannelBroker{
+			taskQueue: make(chan task.Task, 1),
+		}
+
+		b.taskQueue <- tsk
+
+		// Act
+		taskQueue := b.Dequeue()
+
+		// Assert
+		assert.Equal(t, tsk, <-taskQueue)
+	})
+}
