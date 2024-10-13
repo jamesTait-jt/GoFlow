3e8e4a99729faca3c52a487ff6f7ec5bf279b4c0
James Tait <jamesatait12@gmail.com>
2024-10-13 23:54:49 +0100

    Add documentation comments to ChannelBroker

--- broker/channel_broker.go
+++ broker/channel_broker.go
@@ -2,20 +2,29 @@ package broker
 
 import "github.com/jamesTait-jt/goflow/task"
 
+// ChannelBroker is a task broker implementation that wraps a buffered Go channel.
+// It manages task submission and retrieval for the GoFlow framework.
 type ChannelBroker struct {
 	taskQueue chan task.Task
 }
 
+// NewChannelBroker creates a new ChannelBroker with a buffered channel of the
+// specified size. The buffer size determines how many tasks can be queued before
+// further submissions block.
 func NewChannelBroker(bufferSize int) *ChannelBroker {
 	q := make(chan task.Task, bufferSize)
 
 	return &ChannelBroker{taskQueue: q}
 }
 
+// Submit adds a task to the ChannelBroker's queue. If the queue is full, it will
+// block until space is available.
 func (cb *ChannelBroker) Submit(t task.Task) {
 	cb.taskQueue <- t
 }
 
+// Dequeue returns a read-only channel of tasks, allowing workers to retrieve
+// tasks for processing.
 func (cb *ChannelBroker) Dequeue() <-chan task.Task {
 	return cb.taskQueue
 }
